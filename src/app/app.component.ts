81eb3af2aae910fba492e7db8524dc3923d34ba5
Julian Mahecha <jmahecha@belatrixsf.com>
2018-05-25 10:12:03 -0500

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  usernameIsEmpty() {
+    return !!this.username.length;
+  }
+  onResetUsername() {
+    this.username = '';
+  }
 }
