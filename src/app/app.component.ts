4b9166d9d5d33afc057ad67241f1a998eff460d4
Julian Mahecha <jmahecha@belatrixsf.com>
2018-05-28 16:47:00 -0500

    split in components, start data binding with alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{
+    type: 'server',
+    name: 'server 1',
+    content: ' just a test server!'
+  }];
   newServerName = '';
   newServerContent = '';
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 }
