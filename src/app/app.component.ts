1569c62a25ebed4e9c1373df3af9fd905ca05db6
Julian Mahecha <jmahecha@belatrixsf.com>
2018-05-28 11:24:49 -0500

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showParagraph = false;
+  log = [];
+
+  onClick() {
+    this.showParagraph = !this.showParagraph;
+    this.log.push(Math.floor(Date.now()));
+  }
+  getBackgroundColor(l) {
+    return this.log.indexOf(l) > 4 ? 'blue' : '';
+  }
+  getWhiteText(l) {
+    return this.log.indexOf(l) > 4;
+  }
 }
