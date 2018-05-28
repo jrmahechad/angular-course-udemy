385bb74132b7c25adda169f32319b15f2b8a94e4
Julian Mahecha <jmahecha@belatrixsf.com>
2018-05-28 10:47:46 -0500

    Revert "solution"
    This reverts commit 81eb3af2aae910fba492e7db8524dc3923d34ba5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = '';
-  usernameIsEmpty() {
-    return !!this.username.length;
-  }
-  onResetUsername() {
-    this.username = '';
-  }
 }
