dc1c88beaa40382194b525c4ec12564e48001f67
Julian Mahecha <jmahecha@belatrixsf.com>
2018-06-05 13:22:33 -0500

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { CountService } from './services/count.service';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+  activeUsers = [];
+  inactiveUsers = [];
+  activeToInactive = 0;
+  inactiveToActive = 0;
+  constructor(private usersService: UsersService, private countService: CountService) {
+    this.activeUsers = this.usersService.activeUsers;
+    this.inactiveUsers = this.usersService.inactiveUsers;
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+    this.usersService.userSetToInactive.subscribe((id: number) => {
+      this.inactiveUsers.push(this.activeUsers[id]);
+      this.activeUsers.splice(id, 1);
+    });
+
+    this.usersService.userSetToActive.subscribe((id: number) => {
+      this.activeUsers.push(this.inactiveUsers[id]);
+      this.inactiveUsers.splice(id, 1);
+    });
+
+    this.countService.updateCounters.subscribe((data: { activeToInactive: number, inactiveToActive: number }) => {
+       this.activeToInactive = data.activeToInactive;
+       this.inactiveToActive = data.inactiveToActive;
+    });
+
   }
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
-  }
+
+
 }
