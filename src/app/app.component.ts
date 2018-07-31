55f7f62ccf3f6a1cf0bd66d91b5650df99978996
Julian Mahecha <jmahecha@belatrixsf.com>
2018-07-31 15:47:27 -0500

    new httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: 'AIzaSyBQudcQaK6biQlgtn50PHLCb71p4Uk-Kew',
+      authDomain: 'angular-course-udemy.firebaseapp.com',
     });
   }
 
