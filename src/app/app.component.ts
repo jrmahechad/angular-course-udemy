d8747519856fcef9be555eae4530e1da5c43f58c
Julian Mahecha <jmahecha@belatrixsf.com>
2018-08-16 15:17:34 -0500

    advance animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,49 @@ import {
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('normal => highlighted', animate(300)),
+      //transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color':'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ]),
+
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal'
+    this.wildState=== 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal'
+  }
+  onShrink(){
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
@@ -42,4 +74,6 @@ export class AppComponent {
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
   }
+
+
 }
